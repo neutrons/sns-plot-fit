6f05da848feaad045758d3caefcc5d3ba0f21f66
Guernsey <ngy@batty.ornl.gov>
2017-10-10 12:11:25 -0400

    Updated webpack build configs and included unit test folder.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 /* Import Store */
 import store from './store/index.js';
 
+import $ from 'jquery';
+require('bootstrap');
+
 new Vue({
   el: '#app',
+  router,
   store,
   render: h => h(App)
 })
